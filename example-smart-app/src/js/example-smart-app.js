d9e8782b2398c2789f97511f7ed90a94fa3cb1d6
ssilvister3 <56793637+ssilvister3@users.noreply.github.com>
2019-10-21 07:56:21 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -16,7 +16,7 @@
                     query: {
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|3141-9',
+                              'http://loinc.org|29463-7',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
                       }
@@ -38,7 +38,7 @@
           }
 
           var height = byCodes('8302-2');
-          var weight = byCodes('3141-9');
+          var weight = byCodes('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -125,7 +125,7 @@
     $('#lname').html(p.lname);
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
-       $('#weight').html(p.weight);
+    $('#weight').html(p.weight);
     $('#height').html();
     $('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
